d0500f9b890be8dd350f2a1716b493fd856ab529
ViacheslavNovozhilov <vyacheslavnovozhilov@gmail.com>
2024-04-29 13:40:19 +1000

    Добавил поле в класс

--- main123.py
+++ main123.py
@@ -2,5 +2,6 @@ print("Hello world")
 print(1234)
 print("Ангара")
 class Fish:
-    def __init__(self, color):
+    def __init__(self, color, name):
+        self.name = name
         self.color = color
